0e8df10771d3297b9766b47105732ddc03d0af42
jeonill <ekdh523@gmail.com>
2023-01-23 01:51:00 +0900

    KakaoLogin

--- camputBack/src/main/java/camput/MainApplication.java
+++ camputBack/src/main/java/camput/MainApplication.java
@@ -2,8 +2,11 @@ package camput;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class MainApplication {
 
 	public static void main(String[] args) {
